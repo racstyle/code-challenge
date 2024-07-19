f73fb3f9e45100816bab25a89c6c1d63b1c05eda
Rachel Estilo <raystilo4@gmail.com>
2024-07-19 14:13:03 -0500

    RE - sending data + error to front-end

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,6 +1,65 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
 
+// function to display the parsed address
+function displayParsedAddr(parsedAddr) {
+   console.log('running display func');
+
+   console.log(parsedAddr);
+
+   var address_components = parsedAddr['address_components'];
+   var resultsDiv = document.getElementById('address-results');
+   var errorDivPlaceholder = document.getElementById('address-error');
+
+   // if there is an error parsing
+   if (address_components == null) {
+      console.log('nothing');
+      
+      // "parent" div to put error underneath
+      resultsDiv.style.display = 'none';  // hide results table if parsing again
+      
+      // error div
+      var errorDivNew = document.createElement('div');
+      errorDivNew.innerHTML = 'Unable to parse this value due to repeated labels. Our team has been notified of the error.';
+      errorDivNew.setAttribute('id', 'address-error');
+      errorDivNew.setAttribute('class', 'alert alert-danger');
+
+      // add error div to HTML
+      resultsDiv.parentNode.appendChild(errorDivNew);
+
+      return;
+   }
+      
+   // show address type
+   document.getElementById('parse-type').innerText = parsedAddr['address_type'];
+   
+   // add rows to table
+   var addressResultsTable = resultsDiv.getElementsByTagName('tbody')[0];  // get access to table element
+   addressResultsTable.innerHTML = "";    // clear table if parsing again
+   
+   for (let address_part in address_components) {
+      // create new row
+      var newRow = addressResultsTable.insertRow();
+      
+      // column 1: address_part
+      var addrCell = newRow.insertCell();
+      var addrText = document.createTextNode(address_part);
+      addrCell.appendChild(addrText);
+      
+      // column 2: tag
+      var tagCell = newRow.insertCell();
+      var tagText = document.createTextNode(address_components[address_part]);
+      tagCell.appendChild(tagText);
+   }
+   
+   // show the results div + hide the error div if present
+   resultsDiv.style.display = "block";
+   if (errorDivPlaceholder) { errorDivPlaceholder.remove(); }
+
+   return;
+}
+
+
 // select the address form
 const addressForm = document.querySelector('.form');
 
@@ -11,13 +70,18 @@ addressForm.addEventListener('submit', async event => {
    // parse the input to be readable JSON
    const data = new FormData(addressForm);
    var dataObj = {};
-   data.forEach((value, key) => dataObj[key] = value);   // this includes CSRF token, only want address input
-   var parsedInput = dataObj['address'];  // extract the address input
+   data.forEach((value, key) => dataObj[key] = value);   // this includes CSRF token
+   var parsedInput = dataObj['address'];  // we only want the address input
 
    // try sending input to API backend
    try {
-      const res = await fetch(`api/parse?address=${parsedInput}`);
+      const res = await fetch(`api/parse?address=${parsedInput}`) // call the API
+         .then((response) => { return response.json(); } )   // parse the fetched response into readable JSON and return it
+
       console.log(res);
+
+      // display parsed address
+      displayParsedAddr(res);
    }
    catch(err) {
       console.error(err.message);
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -18,12 +18,18 @@ class AddressParse(APIView):
         # parse() method and return the parsed components to the frontend.
         
         input_string = request.GET.get('address')    # get the input from the front-end
+        address_components = {}
+        address_type = ''
+        
         print('request string:', input_string)
         
-        print('running parse:', self.parse(input_string))
-        
+        print('running parse:')
+        address_components, address_type = self.parse(input_string)
+        print(address_components)
+        print(address_type)
+        print('end of parse')
             
-        return Response({'input_string': input_string})
+        return Response({'input_string': input_string, 'address_components': address_components, 'address_type': address_type})
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
@@ -32,23 +38,23 @@ class AddressParse(APIView):
         print('address:', address)
         
         # init
-        address_components = []     # address broken down into components (list)
-        address_type = []           # address component type (list)
-        parsedAddr = {}             # address parsed (dictionary)
+        apiResponse = {}            # USAddress API response (dictionary)
+        address_components = {}     # address parsed broken down into address_part:tag (switched fr init, see below) (dictionary)
+        address_type = ''           # address type (string)
         
         # try parsing the address input
         try:
-            parsedAddr = usaddress.tag(address)[0]
+            apiResponse = usaddress.tag(address)  # parse input
             
-            print(parsedAddr)
-            
-            address_components = list(parsedAddr.values())
-            address_type = list(parsedAddr.keys())
+            address_components = { str(address_part):str(tag) for tag,address_part in dict(apiResponse[0]).items() }     # first item is address broken down, need to switch key:val pair since API returned as tag:address_part
+            address_type = apiResponse[1]         # second item is input type
             
             print('components:', address_components)
             print('type:', address_type)
+            
+            return address_components, address_type
         except:
             print('Error parsing the address input')
+            return None, None
         
-        return address_components, address_type
         
