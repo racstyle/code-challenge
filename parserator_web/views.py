066e2793d7adcb07c8f009c50f40c307b97ea63f
Rachel Estilo <raystilo4@gmail.com>
2024-07-19 17:49:44 -0500

    Resolved majority of lint errors

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -16,45 +16,55 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        
-        input_string = request.GET.get('address')    # get the input from the front-end
+
+        # get the input from the front-end
+        input_string = request.GET.get('address')
+
+        # initializing
         address_components = {}
         address_type = ''
-        
-        print('request string:', input_string)
-        
-        print('running parse:')
+
+        # parse the input
         address_components, address_type = self.parse(input_string)
-        print(address_components)
-        print(address_type)
-        print('end of parse')
-            
-        return Response({'input_string': input_string, 'address_components': address_components, 'address_type': address_type})
+
+        # if there was an error parsing the input
+        if not address_components and not address_type:
+            raise ParseError('An error has occurred')
+
+        # otherwise, return parsed response
+        return Response({
+            'input_string': input_string,
+            'address_components': address_components,
+            'address_type': address_type
+        })
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
-        
+
         print('address:', address)
-        
+
         # init
         apiResponse = {}            # USAddress API response (dictionary)
-        address_components = {}     # address parsed broken down into address_part:tag (switched fr init, see below) (dictionary)
+        address_components = {}     # address broken down as address_part:tag (dictionary)
         address_type = ''           # address type (string)
-        
+
         # try parsing the address input
         try:
             apiResponse = usaddress.tag(address)  # parse input
-            
-            address_components = { str(address_part):str(tag) for tag,address_part in dict(apiResponse[0]).items() }     # first item is address broken down, need to switch key:val pair since API returned as tag:address_part
-            address_type = apiResponse[1]         # second item is input type
-            
+
+            # first item is address broken down,
+            # need to switch key:val pair since API returned as tag:address_part
+            for tag, address_part in dict(apiResponse[0]).items():
+                address_components[str(address_part)] = str(tag)
+
+            # second item is input type
+            address_type = apiResponse[1]
+
             print('components:', address_components)
             print('type:', address_type)
-            
+
             return address_components, address_type
-        except:
+        except Exception:
             print('Error parsing the address input')
             return None, None
-        
-        
