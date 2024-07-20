5d96b12d478b2542cd25e8a21d09587f36ae8447
Rachel Estilo <raystilo4@gmail.com>
2024-07-20 14:54:08 -0500

    RE - Test 1 done (needs further testing)

--- tests/test_views.py
+++ tests/test_views.py
@@ -5,7 +5,26 @@ def test_api_parse_succeeds(client):
     # TODO: Finish this test. Send a request to the API and confirm that the
     # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    print(address_string)
+    
+    expected_output = {
+        'input_string': '123 main st chicago il',
+        'address_components': {
+            '123': 'AddressNumber',
+            'main': 'StreetName',
+            'st': 'StreetNamePostType',
+            'chicago': 'PlaceName',
+            'il': 'StateName'
+        },
+        'address_type': 'Street Address'
+    }
+
+    try:
+        response_read = client.get(f'/api/parse/?address={address_string}', format='json').json()
+        print(response_read)
+        assert response_read == expected_output
+    except Exception:
+        pytest.fail()
 
 
 def test_api_parse_raises_error(client):
